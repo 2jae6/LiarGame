a60c5e46bfec62186c31e98373a05ed94a05615c
LeeJaeWook <2jae66@gmail.com>
2022-04-17 13:02:46 +0900

    앱 프로젝트 생성

---
+++ LiarGame/LiarGame/Sources/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LiarGame
+//
+//  Created by Jay on 2022/04/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.ㄴ
+        self.view.backgroundColor = .brown
+    }
+
+
+}
+
