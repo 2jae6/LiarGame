6f1d20a4117b6efb691f46a934aa22cf0cf1ea0c
LeeJaeWook <2jae66@gmail.com>
2022-04-18 22:42:50 +0900

    FlexLayout 적용 완료

--- LiarGame/Sources/ViewController.swift
+++ LiarGame/Sources/ViewController.swift
@@ -6,15 +6,53 @@
 //
 
 import UIKit
+import PinLayout
+import FlexLayout
+import Then
 
 class ViewController: UIViewController {
 
+    let rootFlexContainer: UIView = UIView()
+    
+    let fView = UIView().then{
+        $0.backgroundColor = .red
+    }
+    let sView = UIView().then{
+        $0.backgroundColor = .green
+    }
+    let tView = UIView().then{
+        $0.backgroundColor = .blue
+    }
+    
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        self.view.addSubview(self.rootFlexContainer)
+        self.rootFlexContainer.flex.paddingTop(12).define { (flex) in
+            
+        }
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.ㄴ
         self.view.backgroundColor = .brown
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.rootFlexContainer.pin.all() // 1)
+        self.rootFlexContainer.flex.layout() // 2)
+        
+    }
 
+    
 
 }
 
