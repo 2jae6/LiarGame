99956e813d42579ee0bb0a45c83be78ced61cc38
elppaaa <withbcd@gmail.com>
2022-04-22 07:36:25 +0900

    feat: Add RandomMusicRepository
    - 랜덤한 뮤직을 제공하는 클래스 정의
    - Google Sheet 를 통해 데이터를 받아옴.
    - Google Sheet 사용을 위한 APIKey 추가 (Secret.xcconfig)
    
     #8

---
+++ LiarGame/Sources/Model/MusicSheetDTO.swift
@@ -0,0 +1,18 @@
+//
+//  MusicSheetDTO.swift
+//  LiarGame
+//
+//  Created by JK on 2022/04/22.
+//
+
+import Foundation
+
+// MARK: - MusicSheetDTO
+struct MusicSheetDTO: Decodable {
+    let range, majorDimension: String
+    let values: [[String]]
+}
+
+
+
+
