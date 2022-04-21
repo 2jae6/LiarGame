29bf196aecce5f2a75f98fb74d39ddce4facfa7f
elppaaa <withbcd@gmail.com>
2022-04-21 13:59:20 +0900

    chore: Add RandomMusizQuiz
    add class frame only

---
+++ LiarGame/Sources/Reactor/RandomMusicQuizReactor.swift
@@ -0,0 +1,24 @@
+//
+//  RandomMusicQuizReactor.swift
+//  LiarGame
+//
+//  Created by JK on 2022/04/21.
+//
+
+import Foundation
+import ReactorKit
+
+final class RandomMusicQuizReactor: Reactor {
+  
+  var initialState = State()
+  
+  enum Action { }
+  enum Mutation { }
+  struct State { }
+  func mutate(action: Action) -> Observable<Mutation> {
+    
+  }
+  func reduce(state: State, mutation: Mutation) -> State {
+    
+  }
+}
